9312c6e22f3cf3b3e63712d4d6382943261aa11a
Sven Janssen <svenjanssen12321@gmail.com>
2022-05-17 14:23:57 +0200

    Fixed a bug where buttons and toggles wouldn't be pressable in the build version

---
+++ Assets/SWToggle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SWToggle : Toggle, I_SmartwallInteractable
+{
+    public void Hit(Vector3 hitpos)
+    {
+        isOn = !isOn;
+    }
+}
