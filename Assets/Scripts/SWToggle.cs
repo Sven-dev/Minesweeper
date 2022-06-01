86229fc1df6c8d364850eca0bd22927987c1cc0a
Sven Janssen <svenjanssen12321@gmail.com>
2022-06-01 15:30:36 +0200

    Fixed a bug where SWToggle would trigger twice when clicking with a mouse
    The solution is a little bit ugly, but works for now

--- Assets/Scripts/SWToggle.cs
+++ Assets/Scripts/SWToggle.cs
@@ -5,8 +5,17 @@ using UnityEngine.UI;
 
 public class SWToggle : Toggle, I_SmartwallInteractable
 {
+#if !UNITY_EDITOR
     public void Hit(Vector3 hitpos)
     {
         isOn = !isOn;
     }
+#endif
+
+#if UNITY_EDITOR
+    public void Hit(Vector3 hitpos)
+    {
+
+    }
+#endif
 }
