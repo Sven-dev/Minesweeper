247e4b53666ec526ba9b300ed9830566a0076cc7
Sven Janssen <svenjanssen12321@gmail.com>
2022-07-19 17:13:49 +0200

    Changed Toggle so the flag icon is transparant instead of invisible when the toggle is not pressed

---
+++ Assets/Scripts/GUIMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UnityEngine.EventSystems.EventTrigger))]
+public class GUIMover : MonoBehaviour
+{
+    [SerializeField] private Vector2 Movement;
+
+    private List<RectTransform> Contents = new List<RectTransform>();
+
+    private void Start()
+    {
+        Contents = transform.GetComponentsInChildren<RectTransform>().ToList();
+        Contents.Remove(GetComponent<RectTransform>());
+    }
+
+    public void MoveContentsDown()
+    {
+        foreach(RectTransform element in Contents)
+        {
+            element.anchoredPosition += Movement;
+        }
+    }
+
+    public void MoveContentUp()
+    {
+        foreach (RectTransform element in Contents)
+        {
+            element.anchoredPosition -= Movement;
+        }
+    }
+}
